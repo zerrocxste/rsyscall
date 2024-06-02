fdf65c7aee9c77e706cbdca00533cb81709c6988
Lobov F. <49279293+zerrocxste@users.noreply.github.com>
2024-06-02 21:10:51 +0600

    add all mmap args & more stability

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include <unistd.h>
 #include <signal.h>
 #include <fcntl.h>
+#include <sys/mman.h>
+#include <optional>
+#include <chrono>
 
 /*
 mov [rsp], rax
@@ -19,28 +22,28 @@ sub rsp, 0x1000
 
 mov rdi, [rsp] # shell_args::addr
 mov rsi, [rsp+0x8] # shell_args::size
-mov rdx, 0x7            # (PROT_READ | PROT_EXEC | PROT_WRITE)
-mov r10, 0x22           # (MAP_PRIVATE | MAP_ANON)
-mov r8, -1            # fd = -1
-xor r9, r9              # off = 0
+mov rdx, [rsp+0x10] # shell_args::prot
+mov r10, [rsp+0x18] # shell_args::flags
+mov r8, [rsp+0x20] # shell_args::fd
+mov r9, [rsp+0x28] #shell_args::offset
 mov rax, 9            # syscall mmap
 syscall
-mov [rsp+0x10], rax # shell_args::mmap_ret
+mov [rsp+0x30], rax # shell_args::mmap_ret
 
-lea rdi, [rsp+0x22] # shell_args::path
+lea rdi, [rsp+0x42] # shell_args::path
 mov rsi, 2
 mov rax, 2        # syscall open
 syscall
 mov r10, rax # save fd
 
 mov rdi, r10
-mov rsi, [rsp+0x18] # shell_args::prologue_shellcode
+mov rsi, [rsp+0x38] # shell_args::prologue_shellcode
 xor rdx, rdx
 mov rax, 8
 syscall
 
 mov rdi, r10
-lea rsi, [rsp+0x20] # shell_args::jmp_inifinite
+lea rsi, [rsp+0x40] # shell_args::jmp_inifinite
 mov rdx, 2
 mov rax, 1       # syscall write
 syscall
@@ -55,7 +58,7 @@ mov rdx, [rsp-0x18]
 mov rcx, [rsp-0x10]
 mov rbx, [rsp-0x8]
 mov rax, [rsp]
-jmp [rsp-0x1000+0x18]*/
+jmp [rsp-0x1000+0x38] #args::shellcode_prologue*/
 
 unsigned char code[] = {
     "\x48\x89\x04\x24"             // mov [rsp], rax
@@ -70,25 +73,25 @@ unsigned char code[] = {
     "\x48\x81\xec\x00\x10\x00\x00" // sub rsp, 0x1000
     "\x48\x8b\x3c\x24"             // mov rdi, [rsp] # shell_args::addr
     "\x48\x8b\x74\x24\x08"         // mov rsi, [rsp+0x8] # shell_args::size
-    "\x48\xc7\xc2\x07\x00\x00\x00" // mov rdx, 0x7            # (PROT_READ | PROT_EXEC | PROT_WRITE)
-    "\x49\xc7\xc2\x22\x00\x00\x00" // mov r10, 0x22           # (MAP_PRIVATE | MAP_ANON)
-    "\x49\xc7\xc0\xff\xff\xff\xff" // mov r8, -1            # fd = -1
-    "\x4d\x31\xc9"                 // xor r9, r9              # off = 0
+    "\x48\x8b\x54\x24\x10"         // mov rdx, [rsp+0x10] # shell_args::prot
+    "\x4c\x8b\x54\x24\x18"         // mov r10, [rsp+0x18] # shell_args::flags
+    "\x4c\x8b\x44\x24\x20"         // mov r8, [rsp+0x20] # shell_args::fd
+    "\x4c\x8b\x4c\x24\x28"         // mov r9, [rsp+0x28] #shell_args::offset
     "\x48\xc7\xc0\x09\x00\x00\x00" // mov rax, 9            # syscall mmap
     "\x0f\x05"                     // syscall
-    "\x48\x89\x44\x24\x10"         // mov [rsp+0x10], rax # shell_args::mmap_ret
-    "\x48\x8d\x7c\x24\x22"         // lea rdi, [rsp+0x22] # shell_args::path
+    "\x48\x89\x44\x24\x30"         // mov [rsp+0x30], rax # shell_args::mmap_ret
+    "\x48\x8d\x7c\x24\x42"         // lea rdi, [rsp+0x42] # shell_args::path
     "\x48\xc7\xc6\x02\x00\x00\x00" // mov rsi, 2
     "\x48\xc7\xc0\x02\x00\x00\x00" // mov rax, 2        # syscall open
     "\x0f\x05"                     // syscall
     "\x49\x89\xc2"                 // mov r10, rax # save fd
     "\x4c\x89\xd7"                 // mov rdi, r10
-    "\x48\x8b\x74\x24\x18"         // mov rsi, [rsp+0x18] # shell_args::prologue_shellcode
+    "\x48\x8b\x74\x24\x38"         // mov rsi, [rsp+0x38] # shell_args::prologue_shellcode
     "\x48\x31\xd2"                 // xor rdx, rdx
     "\x48\xc7\xc0\x08\x00\x00\x00" // mov rax, 8
     "\x0f\x05"                     // syscall
     "\x4c\x89\xd7"                 // mov rdi, r10
-    "\x48\x8d\x74\x24\x20"         // lea rsi, [rsp+0x20] # shell_args::jmp_inifinite
+    "\x48\x8d\x74\x24\x40"         // lea rsi, [rsp+0x40] # shell_args::jmp_inifinite
     "\x48\xc7\xc2\x02\x00\x00\x00" // mov rdx, 2
     "\x48\xc7\xc0\x01\x00\x00\x00" // mov rax, 1       # syscall write
     "\x0f\x05"                     // syscall
@@ -102,7 +105,7 @@ unsigned char code[] = {
     "\x48\x8b\x4c\x24\xf0"         // mov rcx, [rsp-0x10]
     "\x48\x8b\x5c\x24\xf8"         // mov rbx, [rsp-0x8]
     "\x48\x8b\x04\x24"             // mov rax, [rsp]
-    "\xff\xa4\x24\x18\xf0\xff\xff" // jmp [rsp-0x1000+0x18]
+    "\xff\xa4\x24\x38\xf0\xff\xff" // jmp [rsp-0x1000+0x18] #args::shellcode_prologue
 };
 
 #pragma pack(push, 1)
@@ -110,10 +113,14 @@ struct shell_args
 {
     long addr;                                 // 0x0
     long size;                                 // 0x8
-    long mmap_ret;                             // 0x10
-    unsigned long prologue_shellcode;          // 0x18
-    unsigned char jmp_infinite[2]{0xeb, 0xfe}; // 0x20
-    char path[15]{"/proc/self/mem"};           // 0x22
+    long prot;                                 // 0x10
+    long flags;                                // 0x18
+    long fd;                                   // 0x20
+    long offset;                               // 0x28
+    long mmap_ret;                             // 0x30
+    unsigned long prologue_shellcode;          // 0x38
+    unsigned char jmp_infinite[2]{0xeb, 0xfe}; // 0x40
+    char path[15]{"/proc/self/mem"};           // 0x42
 };
 #pragma pack(pop)
 
@@ -148,46 +155,73 @@ int get_pid(const char *process)
                : strtoul(pid_buffer, NULL, 10);
 }
 
-long remote_mmap(int pid, void *address, size_t len)
+bool is_process_paused(char *path_stat)
 {
-    constexpr int MAX_TRY_COUNT = 10000;
+    int fd_stat = open(path_stat, O_RDONLY);
+    char temp[1024]{};
+    read(fd_stat, temp, sizeof(temp));
+    for (int i = 0, sc = 0; i < 50; i++)
+    {
+        if (temp[i] == ' ' && ++sc == 2)
+        {
+            if (temp[i + 1] == 'T' || temp[i + 1] == 't')
+            {
+                close(fd_stat);
+                return true;
+            }
+        }
+    }
+    close(fd_stat);
+    return false;
+}
 
-    int fd;
-    char buffer[4096]{};
+struct pair_rsp_rip
+{
+    int read_ret;
+    std::uintptr_t rsp, rip;
+};
+
+pair_rsp_rip parse_procfs_syscall(int fd_syscall)
+{
     int sysnr{};
     std::uintptr_t regs[8];
-    std::uintptr_t rsp, rip;
+    char buffer[512]{};
+
+    int read_ret = read(fd_syscall, buffer, sizeof(buffer));
+    if (read_ret < 0)
+        return {-errno};
+
+    sscanf(buffer, "%d %lx %lx %lx %lx %lx %lx %lx %lx",
+           &sysnr,
+           &regs[0], &regs[1], &regs[2], &regs[3], &regs[4], &regs[5], &regs[6], &regs[7]);
+
+    for (int i = (sizeof(regs) / sizeof(regs[0])) - 1; i != -1; i--)
+    {
+        if (regs[i] != 0)
+            return {read_ret, regs[i - 1], regs[i]};
+    }
+    return {};
+}
+
+long remote_mmap(int pid, void *address, size_t length, int prot, int flags, int fd, off_t offset)
+{
+    constexpr int MAX_TRY_COUNT = 10000;
+    constexpr int NOT_INITIALIZED = -1000;
+
+    int fd_syscall, fd_mem;
+    char path_stat[4096]{}, path_syscall[4096]{}, path_mem[4096]{};
     unsigned char backup[sizeof(code)];
 
-    kill(pid, SIGSTOP);
+    sprintf(path_stat, "/proc/%d/stat", pid);
+    sprintf(path_syscall, "/proc/%d/syscall", pid);
+    sprintf(path_mem, "/proc/%d/mem", pid);
 
-    sprintf(buffer, "/proc/%d/stat", pid);
+    kill(pid, SIGSTOP);
 
     int try_count = 0;
     while (try_count < MAX_TRY_COUNT)
     {
-        auto is_paused = [&]()
-        {
-            fd = open(buffer, O_RDONLY);
-
-            char temp[1024]{};
-            read(fd, temp, sizeof(temp));
-            for (int i = 0, sc = 0; i < 50; i++)
-            {
-                if (temp[i] == ' ' && ++sc == 2)
-                {
-                    if (temp[i + 1] == 'T' || temp[i + 1] == 't')
-                    {
-                        close(fd);
-                        return true;
-                    }
-                }
-            }
-            close(fd);
-            return false;
-        };
-
-        if (is_paused())
+        if (is_process_paused(path_stat))
             break;
 
         try_count++;
@@ -197,53 +231,41 @@ long remote_mmap(int pid, void *address, size_t len)
     if (try_count >= MAX_TRY_COUNT)
         return -ENOENT;
 
-    sprintf(buffer, "/proc/%d/syscall", pid);
-    fd = open(buffer, O_RDONLY);
-    if (fd <= 0)
+    fd_syscall = open(path_syscall, O_RDONLY);
+    if (fd_syscall <= 0)
         return -ESRCH;
 
-    if (read(fd, buffer, sizeof(buffer)) < 0)
-        return -errno;
+    auto proc_syscall = parse_procfs_syscall(fd_syscall);
 
-    close(fd);
+    if (proc_syscall.read_ret < 0)
+        return proc_syscall.read_ret;
 
-    sscanf(buffer, "%d %lx %lx %lx %lx %lx %lx %lx %lx",
-           &sysnr,
-           &regs[0], &regs[1], &regs[2], &regs[3], &regs[4], &regs[5], &regs[6], &regs[7]);
-
-    for (int i = (sizeof(regs) / sizeof(regs[0])) - 1; i != -1; i--)
-    {
-        if (regs[i] != 0)
-        {
-            rsp = regs[i - 1];
-            rip = regs[i];
-            break;
-        }
-    }
-
-    if (!rsp || !rip)
+    if (!proc_syscall.rsp || !proc_syscall.rip)
         return -EAGAIN;
 
-    sprintf(buffer, "/proc/%d/mem", pid);
-    fd = open(buffer, O_RDWR);
+    fd_mem = open(path_mem, O_RDWR);
 
-    read_memory(fd, rip, backup, sizeof(backup));
-    write_memory(fd, rip, code, sizeof(code));
+    read_memory(fd_mem, proc_syscall.rip, backup, sizeof(backup));
+    write_memory(fd_mem, proc_syscall.rip, code, sizeof(code));
 
     shell_args args{};
     args.addr = 0;
     args.size = 0x1000;
-    args.mmap_ret = -100;
-    args.prologue_shellcode = rip;
-    std::uintptr_t shell_address = rsp - 0x1000;
-    write_memory(fd, shell_address, &args, sizeof(args));
+    args.prot = prot;
+    args.flags = flags;
+    args.fd = fd;
+    args.offset = offset;
+    args.mmap_ret = NOT_INITIALIZED;
+    args.prologue_shellcode = proc_syscall.rip;
+    std::uintptr_t shell_address = proc_syscall.rsp - 0x1000;
+    write_memory(fd_mem, shell_address, &args, sizeof(args));
 
     kill(pid, SIGCONT);
     try_count = 0;
     while (try_count < MAX_TRY_COUNT)
     {
         unsigned char patch[2]{};
-        read_memory(fd, rip, patch, 2);
+        read_memory(fd_mem, proc_syscall.rip, patch, 2);
         if (!memcmp(patch, args.jmp_infinite, 2))
             break;
         try_count++;
@@ -252,15 +274,32 @@ long remote_mmap(int pid, void *address, size_t len)
 
     if (try_count >= MAX_TRY_COUNT)
     {
-        close(fd);
+        close(fd_syscall);
+        close(fd_mem);
         return -EINTR;
     }
 
-    read_memory(fd, shell_address, &args, sizeof(args));
-    write_memory(fd, rip, backup, sizeof(backup));
+    close(fd_syscall);
+    while (true)
+    {
+        fd_syscall = open(path_syscall, O_RDONLY);
+        if (fd_syscall <= 0)
+            return -ESRCH;
+        kill(pid, SIGSTOP);
+        proc_syscall = parse_procfs_syscall(fd_syscall);
+        if (proc_syscall.rip == args.prologue_shellcode)
+            break;
+        kill(pid, SIGCONT);
+        close(fd_syscall);
+        usleep(1000);
+    }
 
-    close(fd);
-    return args.mmap_ret == -100 ? -EIO : args.mmap_ret;
+    read_memory(fd_mem, shell_address, &args, sizeof(args));
+    write_memory(fd_mem, proc_syscall.rip, backup, sizeof(backup));
+
+    close(fd_syscall);
+    close(fd_mem);
+    return args.mmap_ret == NOT_INITIALIZED ? -EIO : args.mmap_ret;
 }
 
 int main(int argc, char **argv)
@@ -278,7 +317,13 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    auto ret = remote_mmap(pid, 0, 4096);
+    auto ts = std::chrono::system_clock::now();
+
+    auto ret = remote_mmap(pid, 0, 4096, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANON, -1, 0);
+
+    std::printf("[!] execution time: %ld microseconds\n",
+                std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - ts).count());
+
     if (ret < 0)
     {
         std::printf("[-] failed. error code: %ld\n", ret);
